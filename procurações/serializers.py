0dc3afc71a14a66f3f14e6dfb41b521d6a618aa7
Prinann <renan.silvajmu@gmail.com>
2025-09-08 19:28:52 -0300

    Delete procurações directory

--- procurações/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Procuracao
-
-class ProcuracaoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Procuracao
-        fields = '__all__'
--- procurações/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets
-from .models import Procuracao
-from .serializers import ProcuracaoSerializer
-
-class ProcuracaoViewSet(viewsets.ModelViewSet):
-    queryset = Procuracao.objects.all()
-    serializer_class = ProcuracaoSerializer
